833a01f0e7152727038ce9c88ff0d00e8e5d2c9e
Rhsti <rhstimeharena9@gmail.com>
2026-09-09 00:58:11 +0200

    Improve mobile responsive design

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -7,40 +7,37 @@ function Header(){
   return(
 
     // header
-    <header class="bg-red-950 py-3">
+    <header  className="bg-red-950 py-3 ">
     {/* nav */}
-    <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
+    <nav className="  mx-auto flex max-w-7xl items-center justify-between py-6 lg:px-8">
     {/* logo */}
-    <div class="flex lg:flex-1">
+    <div className="flex lg:flex-1">
    
-    <img  class="h-16 w-auto size-18
-     rounded-full bg-radial-[at_50%_75%] 
-     from-sky-200 via-blue-400 to-indigo-900 to-90%
-      scale-95 hover:scale-120  "  src={coffeelogo} alt="Coffeeshop Logo" />   
+    <img  src={coffeelogo} alt="Coffeeshop Logo" />   
  </div>
     
       {/* Nav List  */}
-   <ul class="flex flex-col md:flex-row gap-10 " >
-        <li><Link to="#"  class="Link-style">Home</Link></li>
-        <li><Link to="#"  class="Link-style">Menu</Link></li>
-        <li><Link to="/About"  class="Link-style">About</Link></li>
-        <li><Link to="#"  class="Link-style">Location</Link></li>
-        <li><Link to="#"  class="Link-style">Contact</Link></li>
+   <ul className="flex flex-col md:flex-row gap-10 sm:hidden  md:block md:flex" >
+        <li><Link to="#"  className="Link-style">Home</Link></li>
+        <li><Link to="#"  className="Link-style">Menu</Link></li>
+        <li><Link to="/About"  className="Link-style">About</Link></li>
+        <li><Link to="#"  className="Link-style">Location</Link></li>
+        <li><Link to="#"  className="Link-style">Contact</Link></li>
       </ul>
  
       
     </nav>
        {/* mobile menus */}
-     <div class="fixed inset-y-0 right-0  p-6  lg:hidden ">
+     <div className="fixed inset-y-0 right-0  p-6  lg:hidden ">
       
-       <button type="button" command="close" commandfor="mobile-menu" class="-m-2.5 rounded-md p-2.5 text-gray-400">
-              <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" data-slot="icon" aria-hidden="true" class="size-6">
+       <button type="button" command="close" commandfor="mobile-menu" className="-m-2.5 rounded-md p-2.5 text-gray-400">
+              <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" data-slot="icon" aria-hidden="true" className="size-6">
                 <path d="M6 18 18 6M6 6l12 12" stroke-linecap="round" stroke-linejoin="round" />
               </svg>
             </button>
-             <button type="button" command="--toggle" commandfor="mobile-menu" class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-white/5 hover:text-white focus:outline-2 focus:-outline-offset-1 focus:outline-indigo-500">
+             <button type="button" command="--toggle" commandfor="mobile-menu" className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-white/5 hover:text-white focus:outline-2 focus:-outline-offset-1 focus:outline-indigo-500">
 
-          <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" data-slot="icon" aria-hidden="true" class="size-6 in-aria-expanded:hidden">
+          <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" data-slot="icon" aria-hidden="true" className="size-6 in-aria-expanded:hidden">
             <path d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" stroke-linecap="round" stroke-linejoin="round" />
           </svg>
         
--- src/component/herosection/Herosection.jsx
+++ src/component/herosection/Herosection.jsx
@@ -1,17 +1,18 @@
 import Flyingcoffee from '../../assets/flying-cup-of-coffee.png'
 function Herosection(){
     return(
-        <section class=" grid grid-cols-2 bg-red-950 h-130 py-8">
-          <div class=" ml-70 tracking-wide  w-100">
-            <h1 class="text-3xl text-yellow-600 italic " >Best Coffee</h1>
-            <h2  class="text-2xl py-2 text-gray-100">Make your day great with our special coffee!</h2>
-            <p class="py-6 text-gray-300">Welcome to our cote parndise, whare avery bean tels
+        <section className=" grid grid-cols-1 bg-red-950 md:h-130 h-160 py-8
+         md:grid-cols-2 ">
+          <div className=" md:ml-70 tracking-wide  w-100 ml-50">
+            <h1 className="text-3xl text-yellow-600 italic " >Best Coffee</h1>
+            <h2  className="text-2xl py-2 text-gray-100">Make your day great with our special coffee!</h2>
+            <p className="py-6 text-gray-300">Welcome to our cote parndise, whare avery bean tels
               e stury and every cup sporks jey. </p> 
-            <button class="bg-yellow-500 mr-8 p-1 w-28 rounded-full">Order Now</button>
-            <button class=" text-white rounded-full w-29 p-1 border-white border">Contact Us</button>
+            <button className="bg-yellow-500 mr-8 p-1 w-28 rounded-full">Order Now</button>
+            <button className=" text-white rounded-full w-29 p-1 border-white border">Contact Us</button>
             </div>
-          <div class="justify-items-center-safe">
-            <img class="h-100 w-auto" src={Flyingcoffee} alt="flying-cup-of-coffee" />
+          <div className="justify-items-center-safe">
+            <img className="md:h-100 h-80 w-auto m-13 py-10 md:m-0 md:py-0" src={Flyingcoffee} alt="flying-cup-of-coffee" />
           </div>
         </section>
     )
