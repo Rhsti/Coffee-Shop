96ab845a5495069297178726b78e3c9dbb29dc25
Rhsti <rhstimeharena9@gmail.com>
2026-09-16 22:56:49 +0200

    add welcome text and resposive photo gallery to home page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -10,7 +10,7 @@ function NotFound() {
         <section className=" text-amber-950 flex gap-6 flex-col items-center justify-center my-30">
             <h1 className="md:text-8xl text-6xl tracking-widest ">4
                 <img className="inline ml-5  md:w-25 md:h-25 w-20 h-20 rounded-full"
-                 src={coffeeimg} alt="" /> 4</h1>
+                 src={coffeeimg} alt="A cup of coffee" /> 4</h1>
         <p className="md:text-3xl text-2xl">Page not found</p>
         </section>
     </main>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,13 @@
 import Footer from '../../component/footer/Footer'
 import Header from '../../component/header/Header'
 import Herosection from '../../component/herosection/Herosection'
-
+import { FaCoffee, FaHeart } from 'react-icons/fa'
+import buttercroissant from '../../assets/buttercroissant.jpg'
+import tea from '../../assets/tea.jpg'
+import coffee from '../../assets/coffee.jpg'
+import sandwich from '../../assets/sandwich.jpg'
+import smoothie from '../../assets/smoothie.jpg'
+import rezeptsmoothie from '../../assets/rezept-smoothie.jpg'
 function Home() {
   return (
     // <div >
@@ -12,8 +18,57 @@ function Home() {
        <Header/>
         <Herosection/>
       {/* Your page content */}
-      <main className="flex-1">
-       
+      <main className="flex-1 ">
+         <div className='flex flex-col items-center 
+         justify-center mt-10 gap-6 md:text-xl text-lg m-10 '>
+          <h1 className='text-amber-300 md:text-4xl text-xl font-bold uppercase underline italic pb-5'>Welcome to Our Coffee Shop {FaCoffee}</h1>
+          <p className='font-bold '>Good coffee. Great moments. A place to feel at home.</p>
+          <p className='md:max-w-5xl max-w-3xl md:text-center leading-8 md:tracking-wider tracking-normal'>Welcome to our coffee shop, where every cup is made with care and every visit is meant to be enjoyed. From rich, freshly brewed coffee to delicious pastries 
+            and cozy surroundings, we are here to make your day a little better.</p>
+          <p className='md:max-w-5xl max-w-3xl md:text-center leading-8 md:tracking-wider tracking-normal'>Whether you are starting your morning, catching up with friends, or simply taking a
+             moment for yourself,<span className='font-bold'> you are always welcome here.</span></p>
+           <p className='font-bold'>Come in, grab your favorite cup, and stay awhile. {FaCoffee} {FaHeart}</p>
+           <h2 className='mt-10 font-bold text-2xl underline italic uppercase'>gallery</h2>
+         </div>
+        {/*  max-w-100 w-full mx-auto */}
+        <section className='grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2
+         gap-5 justify-center m-10 '>
+          
+          <div className=''>
+            <img className='h-full'  src={buttercroissant} alt="A perfect coffee with buttercroissant." />
+          </div>
+        <div className=''>
+           <img className='h-full'  src={tea} alt="A tasty tea in the cup." />
+
+        </div>
+        <div className=' '>
+           <img className='h-full'  src={coffee} alt="A tasty coffee in the cup." />
+
+        </div>
+         <div className=' '>
+           <img className='h-full'  src={sandwich} alt="Fresh and tasty sandwich." />
+
+         </div>
+        <div className=''>
+          <img className='h-full'  src={smoothie} alt="smoothie with psyllium husks and coconut water." />
+
+        </div>
+           
+        <div className=''>
+          <img className='h-full'  src={rezeptsmoothie} alt="smoothie with banana and berries  and spinach." />
+
+        </div>
+         <div className=' '>
+           <img className='h-full '  src={sandwich} alt="Fresh and tasty sandwich." />
+
+         </div>
+        
+        <div className=''>
+          <img className='h-full'  src={rezeptsmoothie} alt="smoothie with banana and berries  and spinach." />
+
+        </div>
+        
+        </section>
       </main>
 
       <Footer />
